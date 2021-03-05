547237f7973a5ca0ed05c18cf95bae5f4d1a648c
ziping-li <575936902@qq.com>
2021-03-05 17:36:33 +0800

    feat: add loading into store

--- src/store/generate.ts
+++ src/store/generate.ts
@@ -26,10 +26,88 @@ export function generateStores(filesPath: string[], options: ResolvedOptions) {
   return stores;
 }
 
+const LoadingPlugin = `
+  const NAMESPACE = "loading";  // 定义模块名
+  const SHOW = "loading/HIDE" // 显示mutation 同步type
+  const HIDE = "loading/HIDE"
+
+  const createLoadingPlugin = ({
+    namespace = NAMESPACE,
+    includes = [],
+    excludes = []
+  } = {}) => {
+    return store => {
+      if (store.state[namespace]) {
+        throw new Error(
+          'createLoadingPlugin: loading namespace exited in current store'
+        );
+      }
+
+      // new vuex的时候注册一个模块进去
+      store.registerModule(namespace, {
+        namespaced: true,
+        state: {
+          global: false, // 定义全局loading
+          effects: {}
+        },
+        // 同步方法
+        mutations: {
+          SHOW(state, { payload }) {
+            state.global = true;
+            state.effects = {
+              ...state.effects,
+              [payload]: true // 将当前的action 置为true
+            };
+          },
+          HIDE(state, { payload }) {
+            state.global = false;
+            state.effects = {
+              ...state.effects,
+              [payload]: false // 将当前的action 置为false
+            };
+          }
+        }
+      });
+
+      store.subscribeAction({
+        // 发起一个action 之前会走这里
+        before: action => {
+          if (onEffect(action, includes, excludes)) {
+            store.commit(SHOW, { payload: action.type });
+          }
+        },
+        // 发起一个action 之后会走这里
+        after: action => {
+          if (onEffect(action, includes, excludes)) {
+            store.commit(HIDE, { payload: action.type });
+          }
+        }
+      });
+    };
+  };
+
+  // 判断是否要执行
+  function onEffect({ type }, includes, excludes) {
+    if (includes.length === 0 && excludes.length === 0) {
+      return true;
+    }
+
+    if (includes.length > 0) {
+      return includes.indexOf(type) > -1;
+    }
+
+    return excludes.length > 0 && excludes.indexOf(type) === -1;
+  }
+
+  const loadingPlugin = createLoadingPlugin();
+`;
+
 export function generateClientCode(stores: any[]) {
   const names = stores.map((n) => n.name);
 
-  let combineStores: any = {};
+  let combineStores: any = {
+    plugins: ['loadingPlugin'],
+  };
 
   names.forEach((name) => {
     if (name === 'index') {
@@ -37,7 +115,7 @@ export function generateClientCode(stores: any[]) {
     } else {
       combineStores.modules = {
         ...combineStores.modules,
-        [name]: name
+        [name]: name,
       };
     }
   });
@@ -45,7 +123,8 @@ export function generateClientCode(stores: any[]) {
   if (!stores.length) return '';
 
   return `
-    import { createStore } from 'vuex'
+    import { createStore } from 'vuex';
+    ${LoadingPlugin}
     ${stores.map((n) => n.imports).join('\n')}
 
     const store = createStore(${JSON.stringify(combineStores, null, 2)
