8c8665777d2e494f4f0836c83338878430c9082c
ziping-li <575936902@qq.com>
2021-03-07 19:07:52 +0800

    feat: add loading into store

--- src/store/generate.ts
+++ src/store/generate.ts
@@ -27,86 +27,84 @@ export function generateStores(filesPath: string[], options: ResolvedOptions) {
 }
 
 const LoadingPlugin = `
-  const NAMESPACE = "loading";  // 定义模块名
-  const SHOW = "loading/SHOW" // 显示mutation 同步type
-  const HIDE = "loading/HIDE"
+const NAMESPACE = '@@loading'
 
-  const createLoadingPlugin = ({
-    namespace = NAMESPACE,
-    includes = [],
-    excludes = []
-  } = {}) => {
-    return store => {
-      if (store.state[namespace]) {
-        throw new Error(
-          'createLoadingPlugin: loading namespace exited in current store'
-        );
+const createLoadingPlugin = ({
+  namespace = NAMESPACE,
+  includes = [],
+  excludes = []
+} = {}) => {
+  return store => {
+    if (store.state[namespace]) {
+      throw new Error(
+        'createLoadingPlugin: namespace exited in current store'
+      )
+    }
+
+    store.registerModule(namespace, {
+      namespaced: true,
+      state: {
+        global: false,
+        effects: {
+
+        }
+      },
+      mutations: {
+        SHOW(state, { payload }) {
+          state.global = true
+          state.effects = {
+            ...state.effects,
+            [payload]: true
+          }
+        },
+        HIDE(state, { payload }) {
+          state.global = false
+          state.effects = {
+            ...state.effects,
+            [payload]: false
+          }
+        }
       }
+    })
 
-      // new vuex的时候注册一个模块进去
-      store.registerModule(namespace, {
-        namespaced: true,
-        state: {
-          global: false, // 定义全局loading
-          effects: {}
-        },
-        // 同步方法
-        mutations: {
-          SHOW(state, { payload }) {
-            state.global = true;
-            state.effects = {
-              ...state.effects,
-              [payload]: true // 将当前的action 置为true
-            };
-          },
-          HIDE(state, { payload }) {
-            state.global = false;
-            state.effects = {
-              ...state.effects,
-              [payload]: false // 将当前的action 置为false
-            };
-          }
+    store.subscribeAction({
+      before: action => {
+        if (shouldEffect(action, includes, excludes)) {
+          store.commit({ type: namespace + '/SHOW', payload: action.type })
         }
-      });
-
-      store.subscribeAction({
-        // 发起一个action 之前会走这里
-        before: action => {
-          if (onEffect(action, includes, excludes)) {
-            store.commit(SHOW, { payload: action.type });
-          }
-        },
-        // 发起一个action 之后会走这里
-        after: action => {
-          if (onEffect(action, includes, excludes)) {
-            store.commit(HIDE, { payload: action.type });
-          }
+      },
+      after: action => {
+        if (shouldEffect(action, includes, excludes)) {
+          store.commit({ type: namespace + '/HIDE', payload: action.type })
         }
-      });
-    };
-  };
+      },
+      error: action => {
+        if (shouldEffect(action, includes, excludes)) {
+          store.commit({ type: namespace + '/HIDE', payload: action.type })
+        }
+      }
+    })
+  }
+}
 
-  // 判断是否要执行
-  function onEffect({ type }, includes, excludes) {
-    if (includes.length === 0 && excludes.length === 0) {
-      return true;
-    }
-
-    if (includes.length > 0) {
-      return includes.indexOf(type) > -1;
-    }
-
-    return excludes.length > 0 && excludes.indexOf(type) === -1;
+function shouldEffect({ type }, includes, excludes) {
+  if (includes.length === 0 && excludes.length === 0) {
+    return true
   }
 
-  const loadingPlugin = createLoadingPlugin();
+  if (includes.length > 0) {
+    return includes.indexOf(type) > -1
+  }
+
+  return excludes.length > 0 && excludes.indexOf(type) === -1
+}
 `;
 
 export function generateClientCode(stores: any[]) {
   const names = stores.map((n) => n.name);
 
   let combineStores: any = {
-    plugins: ['loadingPlugin'],
+    plugins: ['createLoadingPlugin()'],
   };
 
   names.forEach((name) => {
